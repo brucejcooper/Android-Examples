c0179600ffbbfd35ee351f527bae03234ec809a7
Bruce Cooper <bruce@brucecooper.net>
2011-06-18 17:36:55 +1000

    Altered Widget example to update every second, using AlarmManager

--- WidgetExample/src/com/eightbitcloud/example/widget/ExampleAppWidgetProvider.java
+++ WidgetExample/src/com/eightbitcloud/example/widget/ExampleAppWidgetProvider.java
@@ -1,36 +1,82 @@
+
 package com.eightbitcloud.example.widget;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 
+import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
 
 public class ExampleAppWidgetProvider extends AppWidgetProvider {
-	DateFormat df = new SimpleDateFormat("hh:mm:ss");
+	private static final String LOG_TAG = "ExampleWidget";
 
-	public static String CLOCK_WIDGET_UPDATE = "CLOCK_WIDGET_UPDATE";
+	private static final DateFormat df = new SimpleDateFormat("hh:mm:ss");
+
+	/**
+	 * Custom Intent name that is used by the AlarmManager to tell us to update the clock once per second.
+	 */
+	public static String CLOCK_WIDGET_UPDATE = "com.eightbitcloud.example.widget.8BITCLOCK_WIDGET_UPDATE";
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		super.onReceive(context, intent);
-
+		
 		if (CLOCK_WIDGET_UPDATE.equals(intent.getAction())) {
-			Toast.makeText(context, "onReceiver()", Toast.LENGTH_LONG).show();
+			Log.d(LOG_TAG, "Clock update");
+			// Get the widget manager and ids for this widget provider, then call the shared
+			// clock update method.
+			ComponentName thisAppWidget = new ComponentName(context.getPackageName(), getClass().getName());
+		    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+		    int ids[] = appWidgetManager.getAppWidgetIds(thisAppWidget);
+		    for (int appWidgetID: ids) {
+				updateAppWidget(context, appWidgetManager, appWidgetID);
+		    	
+		    }
 		}
 	}
+	
+	private PendingIntent createClockTickIntent(Context context) {
+        Intent intent = new Intent(CLOCK_WIDGET_UPDATE);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
+        return pendingIntent;
+	}
+	
+	@Override
+	public void onDisabled(Context context) {
+		super.onDisabled(context);
+		Log.d(LOG_TAG, "Widget Provider disabled. Turning off timer");
+    	AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(createClockTickIntent(context));	
+	}
+	
+	@Override 
+	public void onEnabled(Context context) {
+		super.onEnabled(context);
+		Log.d(LOG_TAG, "Widget Provider enabled.  Starting timer to update widget every second");
+        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+    	
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.add(Calendar.SECOND, 1);
+        alarmManager.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), 1000, createClockTickIntent(context));
+	}
+	
+	
+	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 		final int N = appWidgetIds.length;
 
-		Log.i("ExampleWidget",	"Updating widgets " + Arrays.asList(appWidgetIds));
+		Log.d(LOG_TAG, "Updating Example Widgets.");
 
 		// Perform this loop procedure for each App Widget that belongs to this
 		// provider
@@ -43,29 +89,24 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
 
 			// Get the layout for the App Widget and attach an on-click listener
 			// to the button
-			RemoteViews views = new RemoteViews(context.getPackageName(),
-					R.layout.widget1);
+			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget1);
 			views.setOnClickPendingIntent(R.id.button, pendingIntent);
-
-			// To update a label
-			views.setTextViewText(R.id.widget1label, df.format(new Date()));
-
 			// Tell the AppWidgetManager to perform an update on the current app
 			// widget
 			appWidgetManager.updateAppWidget(appWidgetId, views);
+
+
+			// Update The clock label using a shared method
+			updateAppWidget(context, appWidgetManager, appWidgetId);
 		}
 	}
 
 	public static void updateAppWidget(Context context,	AppWidgetManager appWidgetManager, int appWidgetId) {
-		String currentTime = formatter.format(new Date());
-		strWidgetText = currentTime;
+		String currentTime =  df.format(new Date());
 
-		RemoteViews updateViews = new RemoteViews(context.getPackageName(),	R.layout.hellowidget_layout);
-		updateViews.setTextViewText(R.id.widgettext, "[" + String.valueOf(appWidgetId) + "]" + strWidgetText);
+		RemoteViews updateViews = new RemoteViews(context.getPackageName(),	R.layout.widget1);
+		updateViews.setTextViewText(R.id.widget1label, currentTime);
 		appWidgetManager.updateAppWidget(appWidgetId, updateViews);
-
-		Toast.makeText(context,	"updateAppWidget(): " + String.valueOf(appWidgetId) + "\n"+ strWidgetText, Toast.LENGTH_LONG).show();
-
 	}
 
 }
